27cbbc1c04bf73ff3aaf2dbbebe2dece6692779a
enaky <enachi98@gmail.com>
2020-10-29 15:07:02 +0200

    Added sql scripts + python script for executing the sql ones.

---
+++ scripts/sql/visualize_data.sql
@@ -0,0 +1,7 @@
+SELECT * FROM messages;
+SELECT * FROM chats;
+SELECT * FROM friend_requests;
+SELECT * FROM friends;
+SELECT * FROM blocked_users;
+SELECT * FROM user_profiles;
+SELECT * FROM users;
