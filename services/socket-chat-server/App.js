291124331bcdb6d5fb5ed2e9d85ac3e7ca767fe5
enaki <enachi98@gmail.com>
2021-01-04 22:53:49 +0200

    Implemented send data and user Storage in socket-chat-server.

--- services/socket-chat-server/App.js
+++ services/socket-chat-server/App.js
@@ -3,9 +3,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+const io = require('socket.io');
+const moment = require('moment');
+const fetch = require('node-fetch');
 
-const users = {}
+const serverSocket = io(server);
 
 // Routing
 app.use(express.static(path.join(__dirname, 'public')));
@@ -14,23 +16,74 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
+const actives = {}
+const allUsers = {}
+const socketsUidPair = {}
 
-io.on('connection', socket => {
+const messageFields = ["chatId", "ownerId", "text", "imagePath"]
+
+serverSocket.on('connection', socket => {
     console.log("Socket joined " + socket.id);
+    fetch('http://localhost:2020/api/messages')
+        .then(res => res.json()) // expecting a json response
+        .then(json => {
+            console.log(json);
+        });
 
-    socket.on('new-user', name => {
-        console.log("Socket, " + socket.id + " set user " + name);
-        users[socket.id] = name
-        socket.broadcast.emit('user-connected', name)
+    socket.on('establish-connection', data => {
+        console.log("Socket, " + socket.id + " set user " + data.name);
+        actives[data.uid] = "on";
+        allUsers[data.uid] = {
+            "socketId": socket.id,
+            "friendId": data["friendId"]
+        };
+        socketsUidPair[socket.id] = data.uid;
+        socket.broadcast.emit('user-connected', {"uid": data.uid})
     });
-    socket.on('send-chat-message', message => {
-        console.log("Socket, " + socket.id + " send chat " + message);
-        socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
+
+    socket.on('send-chat-message', data => {
+        console.log("Socket, " + socket.id + " set user " + data.name);
+        let validMessage = true;
+        for (let i = 0; i < messageFields.length; i++){
+            if (!data.hasOwnProperty(messageFields[i])){
+                validMessage = false;
+                break;
+            }
+        }
+        if (validMessage){
+            console.log(socketsUidPair[socket.id] + " send a valid message ");
+
+            fetch('http://localhost:2020/api/messages', {
+                method: 'post',
+                body:    JSON.stringify(data),
+                headers: { 'Content-Type': 'application/json' },
+            })
+                .then(res => res.json())
+                .then(json => console.log(json));
+        } else {
+            console.log(socketsUidPair[socket.id] + " send an invalid message ");
+        }
     });
+
     socket.on('disconnect', () => {
-        console.log("Socket, " + socket.id + "disconnected");
-        socket.broadcast.emit('user-disconnected', users[socket.id])
-        delete users[socket.id]
+        const uid = socketsUidPair[socket.id]
+        console.log("(Socket, User): (" + socket.id + ", " + uid + ") disconnected");
+        socket.broadcast.emit('user-disconnected', {"uid": uid})
+        actives[uid] = moment().unix();
+        delete socketsUidPair[socket.id];
+    });
+
+    socket.on('request-active', () => {
+        console.log("Request Actives from " + socket.id);
+        serverSocket.to(socket.id).emit("request-active-response", actives)
+    });
+    socket.on('request-users', () => {
+        console.log("Request Users from " + socket.id);
+        serverSocket.to(socket.id).emit("request-users-response", allUsers)
+    });
+    socket.on('request-sockets', () => {
+        console.log("Request Sockets from " + socket.id);
+        serverSocket.to(socket.id).emit("request-sockets-response", socketsUidPair)
     });
 });
 
--- services/socket-chat-server/public/script.js
+++ services/socket-chat-server/public/script.js
@@ -3,9 +3,25 @@ const messageContainer = document.getElementById('message-container')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
 
-const name = prompt('What is your name?')
+user = {
+    "uid": Date.now().toString()
+}
+
+message = {
+    "chatId": 1,
+    "ownerId": 2,
+    "text": "ajutor",
+    "imagePath": "null"
+}
+
+//const name = prompt('What is your name?')
+const name = "Jeff";
 appendMessage('You joined')
-socket.emit('new-user', name)
+socket.emit('establish-connection', {
+    "uid": user.uid,
+    "name": name,
+    "friendId": "test"
+});
 
 socket.on('chat-message', data => {
     appendMessage(`${data.name}: ${data.message}`)
@@ -23,12 +39,55 @@ messageForm.addEventListener('submit', e => {
     e.preventDefault()
     const message = messageInput.value
     appendMessage(`You: ${message}`)
-    socket.emit('send-chat-message', message)
+    socket.emit('send-chat-message', {
+        "uid": user.uid,
+        "friendId": "test",
+        "message": message
+    })
+    messageInput.value = ''
+})
+
+function ActiveClick(){
+    appendMessage(`You Requested Actives`)
+    socket.emit('request-active')
+}
+socket.on('request-active-response', data => {
+    alert(JSON.stringify(data, null, 4));
+})
+function AllUserClick(){
+    appendMessage(`You Requested All Users`)
+    socket.emit('request-users')
+}
+socket.on('request-users-response', data => {
+    alert(JSON.stringify(data, null, 4));
+})
+function SocketPairClick(){
+    appendMessage(`You Requested Sockets Pairs`)
+    socket.emit('request-sockets')
+}
+socket.on('request-sockets-response', data => {
+    alert(JSON.stringify(data, null, 4));
+})
+function testMessageSend(){
+    appendMessage(`You Sent the test message`);
+    socket.emit('send-chat-message', message);
+}
+
+messageForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const message = messageInput.value
+    appendMessage(`You: ${message}`)
+    socket.emit('send-chat-message', {
+        "uid": user.uid,
+        "friendId": "test",
+        "message": message
+    })
     messageInput.value = ''
 })
 
 function appendMessage(message) {
     const messageElement = document.createElement('div')
+    messageElement.className="row";
     messageElement.innerText = message
     messageContainer.append(messageElement)
 }
